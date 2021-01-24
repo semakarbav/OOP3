fb3509fd7a7a0e5607ff556cdebc1c201a71f4b2
semakarbav <semakarbav06mail.com>
2021-01-24 20:51:04 +0300

    23 ocak

---
+++ KrediBaseManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    interface IKrediBaseManager
+    {//Birbirinin alternatefi olan ama kod içerikleri farklı olan kodlar için interface kullanılır
+        void Hesapla();
+        void BiseyYap();
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //İnterface onu implemente eden classın referans değerini tutabilir:
+
+            IKrediBaseManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediBaseManager konutKredi = new KonutKredi();
+            IKrediBaseManager tasitKrediManager = new TasitKrediManager();
+
+            ILoggerService databaseloggerservice = new DatabaseLoggerService();
+            ILoggerService fileloggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager,fileloggerService);
+
+
+           List<IKrediBaseManager> krediler = new List<IKrediBaseManager>() {ihtiyacKrediManager,tasitKrediManager};
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        }
+    }
+}
